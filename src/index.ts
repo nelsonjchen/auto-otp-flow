28c4f514475e17b372fb843c6e3c3212f1275bc0
Nelson Chen <crazysim@gmail.com>
2024-07-12 15:38:30 -0700

    Initial Email Handler

--- src/index.ts
+++ src/index.ts
@@ -16,15 +16,49 @@ export default {
 		return new Response('Hello World!');
 	},
 	async email(message, env, ctx) {
+		const reader = message.raw.getReader();
+		const chunks = [];
+		let done = false;
+		while (!done) {
+			const { value, done: rDone } = await reader.read();
+			if (value) {
+				chunks.push(value);
+			}
+			done = rDone;
+		}
+		const body = new Uint8Array(chunks.reduce((acc, chunk) => acc + chunk.length, 0));
+		let offset = 0;
+		for (const chunk of chunks) {
+			body.set(chunk, offset);
+			offset += chunk.length;
+		}
+		const bodyText = new TextDecoder().decode(body);
+
+		const headersObj: Record<string, string> = {};
+		message.headers.forEach((value, key) => {
+			headersObj[key] = value;
+		});
+
+		const value = {
+			processedDateTime: new Date().toISOString(),
+			to: message.to,
+			from: message.from,
+			raw: bodyText,
+			headers: headersObj,
+		};
+
+		// Log the email
+		console.log({
+			message: 'Received email',
+			data: {
+				value
+			},
+		});
+
 		// Store the email in the KV namespace
 		await env.AUTO_OTP_FLOW_KV.put(
 			'email:' + message.to,
-			JSON.stringify({
-				from: message.from,
-				raw: message.raw,
-				headers: message.headers,
-				rawSize: message.rawSize,
-			})
+			JSON.stringify(value)
 		);
 	}
 } satisfies ExportedHandler<Env>;
